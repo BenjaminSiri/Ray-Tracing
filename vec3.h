019b9faec553ed66fe1165f35042f4c5533e7add
BenjaminSiri <ben@acsiri.com>
2023-03-15 14:36:03 -0400

    Finished 3.2

--- vec3.h
+++ vec3.h
@@ -55,8 +55,29 @@ using color = vec3;
 
 #endif
 
+
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v){
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v){
+    return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &u){
+    return vec3(u.e[0]*t, u.e[1]*t, u.e[2]*t);
+}
+
+inline vec3 operator*(const vec3 &u, double t){
+    return t * u;
 }
