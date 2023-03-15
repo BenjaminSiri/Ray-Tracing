f5177933855ad665d599f86f6d07aa4878ea3f6f
BenjaminSiri <ben@acsiri.com>
2023-03-15 17:12:29 -0400

    Chapter 5

--- main.cc
+++ main.cc
@@ -5,10 +5,22 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
     color white = color(1.0, 1.0, 1.0);
     color blue = color(0.5, 0.7, 1.0);
 
+    if (hit_sphere(point3(0,0,-1), 0.5, r)){
+        return color(1,0,0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t)*white + t*blue;
--- vec3.h
+++ vec3.h
@@ -85,7 +85,7 @@ class vec3 {
             return (1/t) * v;
         }
 
-        inline double dot(const vec3 &u, const vec3 &v) {
+        friend inline double dot(const vec3 &u, const vec3 &v) {
             return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
         }
 
