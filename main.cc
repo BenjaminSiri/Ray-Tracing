0efe03e603d2bc4882d8f67290c3b695d2c02016
BenjaminSiri <ben@acsiri.com>
2023-03-17 20:41:00 -0400

    Chapter 6

--- main.cc
+++ main.cc
@@ -1,26 +1,20 @@
 // Updated: Chapter 4
+#include "utility.h"
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
-}
-
-color ray_color(const ray& r) {
+color ray_color(const ray& r, const hittable& world) {
     color white = color(1.0, 1.0, 1.0);
     color blue = color(0.5, 0.7, 1.0);
 
-    if (hit_sphere(point3(0,0,-1), 0.5, r)){
-        return color(1,0,0);
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return .5*(rec.normal + color(1,1,1));
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t)*white + t*blue;
@@ -33,6 +27,11 @@ int main(){
     const double aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // camera
 
@@ -53,10 +52,10 @@ int main(){
         //Progress indicator
         std::cerr << "\rScaleLines remaining: " << j << ' ' << std::flush;
         for (int i=0; i < image_width; i++){
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
+            double u = double(i) / (image_width-1);
+            double v = double(j) / (image_height-1);
             ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
--- vec3.h
+++ vec3.h
@@ -8,8 +8,8 @@
 using std::sqrt;
 
 class vec3 {
-    double e[3];
     public:
+        double e[3];
         vec3() {
             e[0] = 0;
             e[1] = 0;
@@ -29,76 +29,118 @@ class vec3 {
         inline double operator[](int i) const { return e[i]; } 
         inline double& operator[](int i) { return e[i]; }
 
-        vec3& operator+=(const vec3 &v) {
-            e[0] += v.e[0];
-            e[1] += v.e[1];
-            e[2] += v.e[2];
-            return *this;
+        inline vec3& operator+=(const vec3 &v2);
+        inline vec3& operator-=(const vec3 &v2);
+        inline vec3& operator*=(const vec3 &v2);
+        inline vec3& operator/=(const vec3 &v2);
+        inline vec3& operator*=(double t);
+        inline vec3& operator/=(double t);
+
+        inline double length() const {
+            return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
         }
 
-        vec3& operator*=(const double t) {
-            e[0] *= t;
-            e[1] *= t;
-            e[2] *= t;
-            return *this;
+        inline double length_squared() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
-        vec3& operator/=(const double t) {
-            return *this *= 1/t;
-        }
-
-        double length_squared() const {
-            return (e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); 
-        }
-
-        double length() const {
-            return sqrt(length_squared());
-        }
-
-        // vec3 Utility Functions
-
-        friend inline std::ostream& operator<<(std::ostream &out, const vec3 &v){
-            return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
-        }
-
-        friend inline vec3 operator+(const vec3 &u, const vec3 &v){
-            return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
-        }
-
-        friend inline vec3 operator-(const vec3 &u, const vec3 &v){
-            return vec3(u.e[0]-v.e[0], u.e[1]-v.e[1], u.e[2]-v.e[2]);
-        }
-
-        friend inline vec3 operator*(const vec3 &u, const vec3 &v){
-            return vec3(u.e[0]*v.e[0], u.e[1]*v.e[1], u.e[2]*v.e[2]);
-        }
-
-        friend inline vec3 operator*(double t, const vec3 &u){
-            return vec3(u.e[0]*t, u.e[1]*t, u.e[2]*t);
-        }
-
-        friend inline vec3 operator*(const vec3 &u, double t){
-            return t * u;
-        }
-
-        friend inline vec3 operator/(vec3 v, double t){
-            return (1/t) * v;
-        }
-
-        friend inline double dot(const vec3 &u, const vec3 &v) {
-            return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
-        }
-
-        inline vec3 cross(const vec3 &u, const vec3 &v) {
-            return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
-                        u.e[2] * v.e[0] - u.e[0] * v.e[2],
-                        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
-        }
-
+        inline void make_unit_vector();
 };
 
+inline std::istream& operator>>(std::istream &is, vec3 &v){
+    is >> v.e[0] >> v.e[1] >> v.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, vec3 &v){
+    os << v.e[0] << " " << v.e[1] << " " << v.e[2];
+    return os;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline vec3 operator*(float t, const vec3 &v) {
+  return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+inline vec3 operator*(const vec3 &v, float t) {
+  return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+inline vec3 operator/(const vec3 &v, float t) {
+  return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+  return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+  return vec3(
+          v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
+          -(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0]),
+          v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
+  );
+}
+
+inline vec3& vec3::operator+=(const vec3 &v) {
+  e[0] += v.e[0];
+  e[1] += v.e[1];
+  e[2] += v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator-=(const vec3 &v) {
+  e[0] -= v.e[0];
+  e[1] -= v.e[1];
+  e[2] -= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator*=(const vec3 &v) {
+  e[0] *= v.e[0];
+  e[1] *= v.e[1];
+  e[2] *= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator/=(const vec3 &v) {
+  e[0] /= v.e[0];
+  e[1] /= v.e[1];
+  e[2] /= v.e[2];
+  return *this;
+}
+
+inline vec3& vec3::operator*=(double t) {
+  e[0] *= t;
+  e[1] *= t;
+  e[2] *= t;
+  return *this;
+}
+
+inline vec3& vec3::operator/=(double t) {
+  float k = 1.0/t;
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+  return *this;
+}
+
 inline vec3 unit_vector(const vec3 &v) {
-    return v / v.length();
+  return v / v.length();
 }
 
 // Type aliases for vec3
